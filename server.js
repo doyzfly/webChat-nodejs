1ef13c5e236752ac337946d59ddcd8af281b23be
luoqiang <lq9328@126.com>
2015-06-27 10:52:13 +0800

    js 整理，服务器端口改为3000

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var online = [];
 var onlineSocket = {};
 // 在8080端口启动服务器
 var server= http.createServer(function(req, res){
-  // 发送HTML的headers和message
-  res.writeHead(200,{ 'Content-Type': 'text/html' });
-  res.end(fs.readFileSync('./index.html'));
+    // 发送HTML的headers和message
+    res.writeHead(200,{ 'Content-Type': 'text/html' });
+    res.end(fs.readFileSync('./index.html'));
 });
 server.listen(3000, function(){
-  console.log('Server started at 3000!');
+    console.log('Server started at 3000!');
 });
 
 // 创建一个Socket.IO实例，把它传递给服务器
@@ -18,27 +18,27 @@ var socket= io.listen(server);
 
 // 添加一个连接监听器
 socket.on('connection', function(client){
-  console.log('客户端连接！');
-  var mark;
-  // 成功！现在开始监听接收到的消息
-  client.on('login',function(name){
-    mark = name;
-    if(online.indexOf(name) == -1){
-      online.push(name);
-    }
-    onlineSocket[name] = client;
-  });
-  client.on('chat',function(data){
-    if(online.indexOf(data.to) != -1){
-      onlineSocket[data.to].emit('to' + data.to, data.msg);
-    }
-  });
-  client.on('message',function(event){
-    console.log('Received message from client!',event);
-  });
-  client.on('disconnect',function(){
-    /*online.splice(online.indexOf(mark),1);
-    delete onlineSocket[mark];
-    console.log(online);console.log(3);*/
-  });
+    console.log('客户端连接！');
+    var mark;
+    // 成功！现在开始监听接收到的消息
+    client.on('login',function(name){
+        mark = name;
+        if(online.indexOf(name) == -1){
+          online.push(name);
+        }
+        onlineSocket[name] = client;
+    });
+    client.on('chat',function(data){
+        if(online.indexOf(data.to) != -1){
+          onlineSocket[data.to].emit('to' + data.to, data.msg);
+        }
+    });
+    client.on('message',function(event){
+        console.log('Received message from client!',event);
+    });
+    client.on('disconnect',function(){
+        /*online.splice(online.indexOf(mark),1);
+        delete onlineSocket[mark];
+        console.log(online);console.log(3);*/
+    });
 });
